b0abce8ca4d5f39d5df11595da6af4442d1da9a2
Nebiyou Shiferaw <nebiyouelias04@gmail.com>
2019-05-10 22:41:32 -0400

    post

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
@@ -9,30 +8,27 @@ const {ShoppingList, Recipes} = require('./models');
 const jsonParser = bodyParser.json();
 const app = express();
 
-// log the http layer
+
 app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
-// so there's some data to look at
+
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
-// adding some recipes to `Recipes` so there's something
-// to retrieve.
+
 Recipes.create(
   'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
 Recipes.create(
   'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
 
-// when the root of this router is called with GET, return
-// all current ShoppingList items
+
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
 app.post('/shopping-list', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
+  
   const requiredFields = ['name', 'budget'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
@@ -44,6 +40,23 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   }
 
   const item = ShoppingList.create(req.body.name, req.body.budget);
+  res.status(201).json(item);
+});
+
+
+
+app.post('/recipes', jsonParser, (req, res) => {
+  
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = Recipes.create(req.body.name, req.body.ingredients);
   res.status(201).json(item);
 });
 
